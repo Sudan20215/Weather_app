ae05521f04c40a5e1f88ff5c9fae1b246169d765
sudan bhusal <sudanbhusal20215@gmail.com>
2020-11-06 08:52:35 -0800

    date defined

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const form = document.querySelector(".sidebar form");
 const input = document.querySelector(".sidebar input");
 const apiKey="668e3d39d52a645e064391881ef3df01"
+function DateNow(){
+    let presentday = new Date();
+    let day = String(presentday.getDate()).padStart(2, '0');
+    let month = String(presentday.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let year = presentday.getFullYear();
+    return "("+month+"/"+day+"/"+year+")"
+}
